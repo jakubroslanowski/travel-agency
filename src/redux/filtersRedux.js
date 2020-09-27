97d7d68b010d89bf5fa98d90ca8367813da7bf45
Jakub Rosłanowski <jakub.roslanowski@gmail.com>
2020-09-27 19:24:04 +0200

    Implement redux

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -1,20 +1,37 @@
 /* SELECTORS */
 
-export const getAllFilters = ({filters}) => filters;
+export const getAllFilters = ({ filters }) => filters;
 
 /* ACTIONS */
 
 // action name creator
 const reducerName = 'filters';
-const createActionName = name => `app/${reducerName}/${name}`;
+const createActionName = (name) => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+export const CHANGE_FROM_DURATION = createActionName('CHANGE_FROM_DURATION');
+export const CHANGE_TO_DURATION = createActionName('CHANGE_TO_DURATION');
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
 // TODO - add other action types
 
 // action creators
-export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+export const changeSearchPhrase = (payload) => ({
+  payload,
+  type: CHANGE_PHRASE,
+});
 // TODO - add other action creators
+export const changeFromDuration = (payload) => ({
+  payload,
+  type: CHANGE_FROM_DURATION,
+});
+export const changeToDuration = (payload) => ({
+  payload,
+  type: CHANGE_TO_DURATION,
+});
+export const addTag = (payload) => ({ payload, type: ADD_TAG });
+export const removeTag = (payload) => ({ payload, type: REMOVE_TAG });
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -25,6 +42,32 @@ export default function reducer(statePart = [], action = {}) {
         searchPhrase: action.payload,
       };
     // TODO - handle other action types
+    case CHANGE_FROM_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          to: action.payload,
+        },
+      };
+    case CHANGE_TO_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          to: action.payload,
+        },
+      };
+    case ADD_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags, action.payload],
+      };
+    case REMOVE_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags.filter((tag) => tag !== action.payload)],
+      };
     default:
       return statePart;
   }
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -12,8 +12,22 @@ export const getFilteredTrips = ({ trips, filters }) => {
   }
 
   // TODO - filter by duration
-
+  if (filters.duration.to && filters.duration.from) {
+    output = output.filter(
+      (trip) =>
+        trip.days >= filters.duration.from && trip.days <= filters.duration.to
+    );
+  }
   // TODO - filter by tags
+  if (filters.tags.length != 0) {
+    output = output.filter((trip) => {
+      for (let tag of trip.tags) {
+        if (filters.tags.indexOf(tag) > -1) {
+          return trip;
+        }
+      }
+    });
+  }
 
   // TODO - sort by cost descending (most expensive goes first)
 
@@ -21,7 +35,7 @@ export const getFilteredTrips = ({ trips, filters }) => {
 };
 
 export const getTripById = ({ trips }, tripId) => {
-  const filtered = trips.filter((trip) => trip.Id == tripId);
+  const filtered = trips.filter((trip) => trip.id == tripId);
 
   // TODO - filter trips by tripId
 
